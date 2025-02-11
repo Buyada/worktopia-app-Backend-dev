95524501fcf4e2573549564aa5bdf3b11030352c
Buyada <110964614+Buyada@users.noreply.github.com>
2025-02-11 23:49:48 +0100

    first commit

---
+++ Worktopia/Worktopia/src/main/java/com/groupe/Worktopia/WorktopiaApplication.java
@@ -0,0 +1,13 @@
+package com.groupe.Worktopia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorktopiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorktopiaApplication.class, args);
+	}
+
+}
---
+++ Worktopia/Worktopia/src/test/java/com/groupe/Worktopia/WorktopiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.groupe.Worktopia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorktopiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
