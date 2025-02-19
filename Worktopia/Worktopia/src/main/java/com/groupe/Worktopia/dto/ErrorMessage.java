d7dfcc4b0e250696ea4c3340b46585775482a6f7
SMART <dongmosammmar@gmail.com>
2025-02-19 18:52:02 +0100

    Ajout ErreurMessage

---
+++ Worktopia/Worktopia/src/main/java/com/groupe/Worktopia/dto/ErrorMessage.java
@@ -0,0 +1,19 @@
+package com.groupe.Worktopia.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Data
+    public class ErrorMessage {
+        private Integer statusCode;
+        private Date timestamp;
+        private String message;
+        private String error;
+    }
+
