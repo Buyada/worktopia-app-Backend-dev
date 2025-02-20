128dafae560efae15675d39f56f639d56aee26ae
SMART <dongmosammmar@gmail.com>
2025-02-20 16:51:47 +0100

    Ajout des exceptions  sur les services formation

--- Worktopia/Worktopia/src/main/java/com/groupe/Worktopia/repository/FormationRepo.java
+++ Worktopia/Worktopia/src/main/java/com/groupe/Worktopia/repository/FormationRepo.java
@@ -3,5 +3,8 @@ package com.groupe.Worktopia.repository;
 import com.groupe.Worktopia.entities.Formation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface FormationRepo extends JpaRepository<Formation, Integer> {
+    Optional<Formation> findByIntitule(String intitule);
 }
--- Worktopia/Worktopia/src/main/java/com/groupe/Worktopia/service/formation/FormationServiceImpl.java
+++ Worktopia/Worktopia/src/main/java/com/groupe/Worktopia/service/formation/FormationServiceImpl.java
@@ -1,15 +1,19 @@
 package com.groupe.Worktopia.service.formation;
 
 import com.groupe.Worktopia.entities.Formation;
+import com.groupe.Worktopia.exception.ResourceExistException;
+import com.groupe.Worktopia.exception.ResourceNotFoundException;
 import com.groupe.Worktopia.repository.FormationRepo;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class FormationServiceImpl implements FormationServer{
     public final FormationRepo formationRepo;
+
     public FormationServiceImpl(FormationRepo formationRepo){
         this.formationRepo = formationRepo;
     }
@@ -17,13 +21,18 @@ public class FormationServiceImpl implements FormationServer{
 
     @Override
     public void addFormation(Formation formation) {
+
+        Optional<Formation> formationToAdd = this.formationRepo.findByIntitule(formation.getIntitule());
+        if(formationToAdd.isPresent())
+            throw new ResourceExistException("Resource already exist!");
         formation.setCreatedAt(new Date());
         this.formationRepo.save(formation);
     }
 
     @Override
     public Formation getFormationById(Integer formationId) {
-        return this.formationRepo.findById(formationId).get();
+        return this.formationRepo.findById(formationId)
+                .orElseThrow(()-> new ResourceNotFoundException("Record not found !"));
     }
 
     @Override
@@ -33,7 +42,8 @@ public class FormationServiceImpl implements FormationServer{
 
     @Override
     public void updateFormationById(Integer formationId, Formation formation) {
-        Formation formationToUpdate = this.formationRepo.findById(formationId).get();
+        Formation formationToUpdate = this.formationRepo.findById(formationId)
+                .orElseThrow(()-> new ResourceNotFoundException("Record to update not found !"));
         formationToUpdate.setIntitule(formation.getIntitule());
         formationToUpdate.setDescription(formation.getDescription());
         formationToUpdate.setDuree(formation.getDuree());
@@ -44,7 +54,10 @@ public class FormationServiceImpl implements FormationServer{
 
     @Override
     public void deleteFormationById(Integer formationId) {
-        this.formationRepo.deleteById(formationId);
+
+        Formation formationToDelete = this.formationRepo.findById(formationId)
+                .orElseThrow(()-> new ResourceNotFoundException("Record to delete not found !"));
+        this.formationRepo.delete(formationToDelete);
     }
 
 
