3140cf47222eaf18b249953d2bba4cbb611b5de3
weezy <kolomsoakossebe@gmail.com>
2025-02-22 16:02:37 +0100

    profil

--- Worktopia/Worktopia/src/main/java/com/groupe/Worktopia/entities/Profil.java
+++ Worktopia/Worktopia/src/main/java/com/groupe/Worktopia/entities/Profil.java
@@ -1,2 +1,33 @@
-package com.groupe.Worktopia.entities;public class Profil {
+package com.groupe.Worktopia.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "Users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Profil implements Serializable {
+
+@Serial
+    private  static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer profilId;
+    private String Username;
+    @Lob
+    @Column(name = "cv")
+    private byte[] cv;  // Stocke le CV en base
+
+    @OneToOne(mappedBy = "profil")
+    private User user;
+
 }
--- Worktopia/Worktopia/src/main/java/com/groupe/Worktopia/entities/User.java
+++ Worktopia/Worktopia/src/main/java/com/groupe/Worktopia/entities/User.java
@@ -1,4 +1,45 @@
 package com.groupe.Worktopia.entities;
-public class User {
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.ScrollableResults;
+import org.hibernate.event.spi.PreInsertEvent;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Date;
+
+@Entity
+@Table(name = "Users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class User implements Serializable {
+    @Serial
+    private  static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer UserId;
+    private  String Firstname;
+    private String lastname;
+    private String email;
+    private String MotdePasse;
+    private Date creatAT;
+    private  Date updateAt;
+    private Boolean statut;
+
+    @ManyToOne
+    @JoinColumn(name = "role_id", nullable = false)
+    private UserRole role;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "profil_id", referencedColumnName = "id")
+    private Profil profil;
     
 }
