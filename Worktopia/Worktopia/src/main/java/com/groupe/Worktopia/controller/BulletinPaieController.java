34997813a0b4ac0fd0ce7b9f028e8a8548063d7f
Buyada <rbuyada@gmail.com>
2025-03-01 00:27:34 +0100

    ;
    employe controller

--- Worktopia/Worktopia/src/main/java/com/groupe/Worktopia/controller/BulletinPaieController.java
+++ Worktopia/Worktopia/src/main/java/com/groupe/Worktopia/controller/BulletinPaieController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.nio.file.Path;
 import java.util.List;
 
+@RestController
 public class BulletinPaieController {
 
     private EmployeRepo employeRepo;
--- Worktopia/Worktopia/src/main/java/com/groupe/Worktopia/controller/EmployeController.java
+++ Worktopia/Worktopia/src/main/java/com/groupe/Worktopia/controller/EmployeController.java
@@ -1,4 +1,65 @@
 package com.groupe.Worktopia.controller;
 
+
+import com.groupe.Worktopia.entities.Employe;
+import com.groupe.Worktopia.repository.EmployeRepo;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
 public class EmployeController {
+    private EmployeRepo employeRepo;
+
+    public EmployeController(EmployeRepo employeRepo){
+        this.employeRepo = employeRepo;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Employe>> getAllEmploye(){
+        return ResponseEntity.status(200).body(this.employeRepo.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Employe> findEmployeById(@PathVariable Long id){
+        Employe employe = this.employeRepo.findById(id).orElseThrow(()->new RuntimeException("employe non trouve"));
+
+        return ResponseEntity.status(200).body(employe);
+    }
+
+    @PostMapping
+    public ResponseEntity<Employe> createEmploye(@RequestBody Employe employe){
+        this.employeRepo.save(employe);
+
+        return ResponseEntity.status(200).body(employe);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Employe> updateEmploye(@PathVariable Long id, @RequestBody Employe employe){
+        this.employeRepo.findById(id).orElseThrow(()->new RuntimeException("employe non trouver !"));
+
+        employe.setFirstname(employe.getFirstname());
+        employe.setLastname(employe.getLastname());
+        employe.setPoste(employe.getPoste());
+        employe.setSalaireBase(employe.getSalaireBase());
+        employe.setPoste(employe.getPoste());
+
+        this.employeRepo.save(employe);
+
+        return ResponseEntity.status(200).body(employe);
+    }
+
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteEmploye(@PathVariable Long id){
+        Employe employe = this.employeRepo.findById(id).orElseThrow(()->new RuntimeException("employe non trouve !"));
+
+        this.employeRepo.delete(employe);
+
+        return ResponseEntity.status(200).body("employe supprimer avec succes !");
+
+    }
+
+    
 }
