bcf0bf56e41b4e111270bcaafac911f9a192503c
SMART <dongmosammmar@gmail.com>
2025-03-16 00:48:15 +0100

    ajout du sort dans le pageRequesDTO

--- Worktopia/Worktopia/src/main/java/com/groupe/Worktopia/dto/PageRequestDTO.java
+++ Worktopia/Worktopia/src/main/java/com/groupe/Worktopia/dto/PageRequestDTO.java
@@ -3,6 +3,7 @@ package com.groupe.Worktopia.dto;
 import lombok.Data;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
 import java.util.Objects;
 
@@ -11,12 +12,16 @@ import java.util.Objects;
 public class PageRequestDTO {
     private Integer pageNo = 0;
     private Integer pageSize = 5;
+    private Sort.Direction sort = Sort.Direction.ASC;
+    private String sortByColumn = "id";
+
 
     public Pageable getPageable(PageRequestDTO pageRequestDTO){
         Integer page = Objects.nonNull(pageRequestDTO.getPageNo()) ? pageRequestDTO.getPageNo() : this.pageNo;
         Integer size = Objects.nonNull(pageRequestDTO.getPageSize()) ? pageRequestDTO.getPageSize() : this.pageSize;
-
-        PageRequest request = PageRequest.of(page, size);
+        Sort.Direction sort = Objects.nonNull(pageRequestDTO.getSort()) ? pageRequestDTO.getSort() : this.sort;
+        String sortByColumn = Objects.nonNull(pageRequestDTO.getSortByColumn()) ? pageRequestDTO.getSortByColumn() : this.sortByColumn;
+        PageRequest request = PageRequest.of(page, size, sort, sortByColumn);
         return request;
     }
 }
