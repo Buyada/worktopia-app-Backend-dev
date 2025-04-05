4169d25dd0081ce626ba77bad6e7a6428541c87c
weezy <kolomsoakossebe@gmail.com>
2025-04-05 18:31:43 +0100

    ok

--- Worktopia/Worktopia/src/main/java/com/groupe/Worktopia/exception/ResourceExistException.java
+++
@@ -1,7 +0,0 @@
-package com.groupe.Worktopia.exception;
-
-public class ResourceExistException extends RuntimeException{
-    public ResourceExistException(String message) {
-        super(message);
-    }
-}
